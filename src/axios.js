043625f7b082cdd038dff1be77ac1d6fdf0a71b9
psmdevgit <mis@pothysswarnamahal.com>
2026-04-02 15:13:44 +0530

    main

--- src/axios.js
+++ src/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-   // baseURL: "http://localhost:8000/api",  
-    baseURL: "http://192.168.5.13:4488/api",  
+   baseURL: "http://localhost:8000/api",  
+    // baseURL: "http://192.168.5.13:4488/api",  
     headers: {
         "Content-Type": "application/json"
     }
